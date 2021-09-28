d658f4446a8595ef295089a44b695e15cec3be99
bess_days <bess8610@gmail.com>
2021-09-27 18:34:33 -0700

    step 1 -> master

--- ling-150/setUp.py
+++ ling-150/setUp.py
@@ -1,27 +1,44 @@
-#this page will input phonetic instructions
-
-
-alveolar = ['d' ,'n','r','ch','ll', 't']
+alveolar = ['d', 'n','l', 't']
 doublecon = ['ll', 'zz', 'ff', 'dd']
-velar = ['k', 'c' , 'g','x']
+velar = ['k', 'c', 'g', 'x']
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = 'fat'
 
-def adjectives(word):
+def endings(word):
+    if word.endswith(tuple(alveolar)) and len(word) > 3 and not word.endswith('l' or 'n' or 'r' or 't'):
+        print(word + 'el')
+        #why these paramaters? 'rel' is a root for religion, relationship, nel isn't a common lexicon combiniation, since 'l' could be double or would be a VcVC which would give the final 'e' a different vowel and  the'l' sound
     #words, specifically adjectives with 3 letters tend to have a CVC pattern with a short vowel in the middle
-    #based on that, I will expand the vowel to a :i or ae depending on the manner of articulation
-    if len(word) == 3 and not word.endswith(tuple(alveolar)):
-        print(word.replace(word[1],'ee',1))
-        #I made alveolar false because many words that end in alveolars can contian the rather rare double letters
     elif len(word) == 3 and word.endswith(tuple(alveolar)):
         print(word.replace(word[1], 'ai', 1))
-    elif word.endswith(tuple(alveolar)):
-        print(word + 'an')
-        #I chose 'an' because 'en' can result in the addition of meaning like 'thick-en'
-    elif word.endswith(tuple(doublecon)):
-        print(word + 'in')
-        #I chose 'in' for double constants cause it is common use in latin languages to add more meaning to words
-    elif word.endswith(tuple(velar)):
+    elif word[1] in list(vowels) and len(word) == 3 and not word.endswith(tuple(vowels)):
+        print(word[:2] + ':' + 'rn')
+        # based on that, I will expand the vowel to a :i or ae depending on the manner of articulation
+    elif word.endswith(tuple(velar)) and len(word) > 3 and not word.endswith('k'):
         print(word + 'on')
+    elif word.endswith(tuple(velar)) and len(word) > 3 and word.endswith('ck'):
+        print(word.replace('ck', ':kern',1))
+    elif word.endswith(tuple(vowels)) and len(word) > 3 and word.endswith('o' or 'a' or 'e'):
+        print(word + 'rn')
+    elif word.endswith(tuple(doublecon)):
+        print(word + 'an')
+    elif word.endswith(tuple(alveolar)) and len(word) == 3 and not word.endswith('l' or 'n' or 'r'):
+        print(word.replace(word[2], 'nt' , 1))
+        print(word.replace(word[1], 'ee',1))
+    elif word.endswith(tuple(alveolar)) and len(word) == 3 and word.endswith('l' or 'n' or 'r'):
+        print(word + 'nar')
+        print(word.replace(word[1], 'e', 1))
+    elif 'ea' in word:
+        print(word.replace('ea', 'ai',1))
 
 
-adjectives("basic")
-#Type word between the ()
+#other rules I could not code: if resembles current english word - insert a different stress or stop'
+#does not keep the first vowel - either makes it longer it or makes it a dipthong
+
+
+endings("suck")
+
+
+
+
+
