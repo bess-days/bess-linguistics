1796b50c728dd1fd38103ba922545a8ba677e3ee
bess_days <bess8610@gmail.com>
2021-09-27 20:49:38 -0700

    added rules and citations

--- ling-150/step1Swadesh.py
+++ ling-150/step1Swadesh.py
@@ -1,19 +1,16 @@
-alveolar = ['d', 'n','l', 't']
+alveolar = ['d', 'n','l', 't', 's']
 doublecon = ['ll', 'zz', 'ff', 'dd']
 velar = ['k', 'c', 'g', 'x']
 vowels = ['a', 'e', 'i', 'o', 'u']
 word = 'fat'
 
 def lexicon(word):
-    if word.endswith(tuple(alveolar)) and len(word) > 3 and not word.endswith('l' or 'n' or 'r' or 't'):
+    if word.endswith(tuple(alveolar)) and len(word) > 3:
         print(word + 'el')
-        #why these paramaters? 'rel' is a root for religion, relationship, nel isn't a common lexicon combiniation, since 'l' could be double or would be a VcVC which would give the final 'e' a different vowel and  the'l' sound
-    #words, specifically adjectives with 3 letters tend to have a CVC pattern with a short vowel in the middle
     elif len(word) == 3 and word.endswith(tuple(alveolar)):
         print(word.replace(word[1], 'ai', 1))
     elif word[1] in list(vowels) and len(word) == 3 and not word.endswith(tuple(vowels)):
         print(word[:2] + ':' + 'rn')
-        # based on that, I will expand the vowel to a :i or ae depending on the manner of articulation
     elif word.endswith(tuple(velar)) and len(word) > 3 and not word.endswith('k'):
         print(word + 'on')
     elif word.endswith(tuple(velar)) and len(word) > 3 and word.endswith('ck'):
@@ -33,10 +30,10 @@ def lexicon(word):
 
 
 #other rules I could not code: if resembles current english word - insert a different stress or stop'
-#does not keep the first vowel - either makes it longer it or makes it a dipthong
 
 
-lexicon()
+
+lexicon("kill")
 #insert word above
 
 
