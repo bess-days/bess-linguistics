74b5f5e3dd389e88df1c7204015d8d7633d2a660
bess_days <bess8610@gmail.com>
2021-10-11 15:39:00 -0700

    working on a return sequence to include all steps

--- ling-150/Swadesh.py
+++ ling-150/Swadesh.py
@@ -1,5 +1,5 @@
 
-word = 'dick'
+word = 'suck'
 
 alveolar = ['d', 'n','l', 't', 's']
 doublecon = ['ll', 'zz', 'ff', 'dd']
@@ -13,35 +13,34 @@ ck_end = word.replace('ck','k',1)
 
 
 def lexicon(word):
-    if word.endswith(tuple(alveolar)) and len(word) > 3:
-        print(word + syllabic[2] )
-    elif len(word) == 3 and word.endswith(tuple(alveolar)):
-        print(word.replace(word[1], 'ai', 1))
-    elif word[1] in list(vowels) and len(word) == 3 and not word.endswith(tuple(vowels)):
-        print(word[:2] + ':' + syllabic[2])
-    elif word.endswith(tuple(velar)) and len(word) > 3 and not word.endswith('k'):
-        print(word + syllabic[0])
-    elif word.endswith(tuple(velar)) and len(word) > 3 and word.endswith('ck'):
-        print(ck_end + syllabic[1])
-    elif word.endswith(tuple(vowels)) and len(word) > 3 and word.endswith('o' or 'a' or 'e'):
-        print(word + syllabic[0])
-    elif word.endswith(tuple(doublecon)):
-        print(word + 'an')
-    elif word.endswith(tuple(alveolar)) and len(word) == 3 and not word.endswith('l' or 'n' or 'r'):
-        print(word.replace(word[2], 'nt' , 1))
-        print(word.replace(word[1], 'ee',1))
-    elif word.endswith(tuple(alveolar)) and len(word) == 3 and word.endswith('l' or 'n' or 'r'):
-        print(word + 'ᶏ')
-        print(word.replace(word[1], 'e', 1))
-    elif 'ea' in word:
-        print(word.replace('ea', 'ai',1))
-
-
+    for w in word:
+        if word.endswith(tuple(alveolar)) and len(word) > 3:
+            print(word + syllabic[2] )
+        elif len(word) == 3 and word.endswith(tuple(alveolar)):
+            print(word.replace(word[1], 'ai', 1))
+        elif word[1] in list(vowels) and len(word) == 3 and not word.endswith(tuple(vowels)):
+            print(word[:2] + ':' + syllabic[2])
+        elif word.endswith(tuple(velar)) and len(word) > 3 and not word.endswith('k'):
+            print(word + syllabic[0])
+        elif word.endswith(tuple(velar)) and len(word) > 3 and word.endswith('ck'):
+            print(ck_end + syllabic[1])
+        elif word.endswith(tuple(vowels)) and len(word) > 3 and word.endswith('o' or 'a' or 'e'):
+            print(word + syllabic[0])
+        elif word.endswith(tuple(doublecon)):
+            print(word + 'an')
+        elif word.endswith(tuple(alveolar)) and len(word) == 3 and not word.endswith('l' or 'n' or 'r'):
+            print(word.replace(word[2], 'nt' , 1))
+            print(word.replace(word[1], 'ee',1))
+        elif word.endswith(tuple(alveolar)) and len(word) == 3 and word.endswith('l' or 'n' or 'r'):
+            print(word + 'ᶏ')
+            print(word.replace(word[1], 'e', 1))
+        elif 'ea' in word:
+            print(word.replace('ea', 'ai',1))
+    return  word
 #other rules I could not code: if resembles current english word - insert a different stress or stop'
 
 
-
-lexicon("dick")
+lexicon("suck")
 #insert word above
 
 
