a119703b7be66ad5a9e068da6c16b3bfb657ce0c
bess_days <bess8610@gmail.com>
2021-10-22 15:36:17 -0700

    struggling with 6 character plus adaptions, however, word meaning added

--- ling-150/step2.py
+++ ling-150/step2.py
@@ -1,3 +1,4 @@
+import pyperclip as pc
 #TODO add /i/ extended for every /i/
 #TODO Every vowel after another vowel becomes an /i/
 #TODO if word ends in vowel and swedish syllabic
@@ -90,11 +91,15 @@ for w in word:
                     word = word + syllabic[2]
             #ends with alveolar
             if word.endswith(tuple(alveolar)):
+                if word[2] in vowels and word[3] in vowels:
+                    print('hi')
+                    word = word[:3] + ':' + word[4:]
+
                 if word[1] in vowels and word[2] in alveolar or velar:
                     word = word[:2] + ':' + word[3:] + syllabic[2]
                     break
                 else:
-                    word = word[:2] + ':' + word[2:] + syllabic[2]
+                        word = word[:3] + ':' + word[4:] + syllabic[2]
             #ends with velar
             if word.endswith(tuple(velar)):
                 if word[1] in vowels:
@@ -120,7 +125,9 @@ for w in word:
             else:
                 word = word + 'g?' + syllabic[2]
 
-
+for w in word:
+    if word[1] in vowels and word[3] in vowels:
+        word = word.replace(word[3], 'g?',1)
 print('Below is the IPA:')
 
 word = word.replace('tS', 'ʧ')
@@ -135,8 +142,13 @@ word = word.replace('l>', 'ɭ')
 word = word.replace('g?', 'ʔ')
 word = word.replace('y', '')
 
-print('[' + word + ']')
+IPA = ('[' + word + ']')
+print(IPA)
+pc.copy(IPA)
 word = list(word)
+
+#Adding aditional vowel changing rules
+
 # Checks wether applicable
 for i in range(len(word)):
     for i in range(len(word)):
@@ -149,3 +161,4 @@ for i in range(len(word)):
             break
         else:
             pass
+
